cdc808a00d2778576ba14106e5b28f8494fbc47d
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-05-29 22:03:50 +0300

    Revert "."
    This reverts commit bbe35b4b48581f576dfb0d27e128eb451ca8353a.

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,74 +1,14 @@
-#!/bin/bash
-# Usage: ./wait-for-it.sh host:port [-s] [-t timeout] [-- command args]
-# Waits for host:port to become available, or until timeout is reached
+#!/bin/sh
+# wait-for-it.sh
 
-set -e
+host="$1"
+shift
+cmd="$@"
 
-host="postgres"
-port="5432"
-timeout=30
-cmd=""
-quiet_mode=""
-
-# Parse command line arguments
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
-        --)
-        shift
-        cmd="$@"
-        break
-        ;;
-        -t)
-        timeout="$2"
-        if [[ ! "$timeout" =~ ^[0-9]+$ ]]; then
-            echo "Error: Invalid timeout value: $timeout" >&2
-            exit 1
-        fi
-        shift 2
-        ;;
-        -q)
-        quiet_mode="true"
-        shift 1
-        ;;
-        *)
-        echo "Error: Invalid argument: $1" >&2
-        exit 1
-        ;;
-    esac
+until nc -z "$host" 5432; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
 
-# Function to check if the port is open
-wait_for_port() {
-    local host="$1"
-    local port="$2"
-    local timeout="$3"
-
-    local start_time=$(date +%s)
-    local end_time=$((start_time + timeout))
-
-    while true; do
-        nc -z "$host" "$port" >/dev/null 2>&1
-        result=$?
-        if [[ $result -eq 0 ]]; then
-            if [[ -n "$quiet_mode" ]]; then
-                return 0
-            else
-                echo "Success: $host:$port is available after $(( $(date +%s) - start_time )) seconds" >&2
-            fi
-            return 0
-        fi
-        current_time=$(date +%s)
-        if [[ "$current_time" -ge "$end_time" ]]; then
-            echo "Error: Timeout waiting for $host:$port" >&2
-            exit 1
-        fi
-        sleep 1
-    done
-}
-
-# Execute the wait_for_port function
-wait_for_port "$host" "$port" "$timeout"
-
-# Execute the provided command, if any
-exec "$cmd"
+>&2 echo "Postgres is up - executing command"
+exec $cmd
