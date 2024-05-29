6a5c225413420dbf1283ad99cffe6793d53d95ff
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-05-29 22:12:22 +0300

    .

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM amazoncorretto:17-alpine-jdk
 COPY target/*.jar app.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","/app.jar"]
--- target/generated-sources/annotations/ru/xorochki/resSearch/dto/CriteriaConverterImpl.java
+++ target/generated-sources/annotations/ru/xorochki/resSearch/dto/CriteriaConverterImpl.java
@@ -6,7 +6,7 @@ import ru.xorochki.resSearch.model.Criteria;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-29T13:36:10+0300",
+    date = "2024-05-29T22:10:07+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
