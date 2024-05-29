98186469ea297359d5d60eeccc01bd71218fad5f
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-05-29 22:03:52 +0300

    Revert "."
    This reverts commit 76f2f7df65588d9d371d4098d901ff0d65d844dc.

--- target/generated-sources/annotations/ru/xorochki/resSearch/dto/UserConverterImpl.java
+++ target/generated-sources/annotations/ru/xorochki/resSearch/dto/UserConverterImpl.java
@@ -8,7 +8,7 @@ import ru.xorochki.resSearch.model.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-29T13:49:56+0300",
+    date = "2024-05-29T13:36:10+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
